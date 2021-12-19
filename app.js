c4e5182fd472b573d6538c6f9e1e2dc711b1a376
mohamed0228 <mohamed0228@outlook.fr>
2021-12-19 16:14:24 -0500

    Create Project Section HTMl

--- app.js
+++ app.js
@@ -125,15 +125,15 @@ Add a New Project
       }
     });
 };
-const pageHTML = generatePage(mockData);
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    
-    // will be uncommented in lesson 4
-     const pageHTML = generatePage(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
     fs.writeFile('./index.html', pageHTML, err => {
-       if (err) throw new Error(err);
-       console.log('Page created! Check out index.html in this directory to see it!');
-     });
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
